30c73f6c624f69821304c87b28911b3f735d63f7
mrjeunegithub <velto25112005@gmail.com>
2025-11-06 14:15:05 +0100

    Update electeurs.h

--- include/electeurs.h
+++ include/electeurs.h
@@ -17,7 +17,16 @@ typedef struct {
     int a_vote; // 0 = non, 1 = oui
 } Electeur;
 
+typedef struct {
+    Electeur aVoter[100];
+    int i;// indice du tableau
+    Electeur naPasVote[100];
+    int j;// indice du tableau
+} Votant;
+
 void genererID(int *id);
 void enregistrerElecteur();
+Votant ontVote(Electeur tab[], int taille);
+int aVote(Electeur tab[], int taille, char nom[], char prenom[]);
 
 #endif
