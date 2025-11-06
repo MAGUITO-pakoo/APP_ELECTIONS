43fbb32d61cef3e5d4ff60e9d92fb02a66d00305
mrjeunegithub <velto25112005@gmail.com>
2025-11-06 14:19:33 +0100

    j'ai alouté mon main en commentaire pour que tu puisses savoir comment exploité facilement

--- src/main.c
+++ src/main.c
@@ -50,3 +50,46 @@ int main(){
     return 0;
     
 }
+
+
+
+
+/* int main() {
+     Electeur electeurs[] = {
+        {"POKEM", "Dane", 1, 2, "Nkolbisson", 19},
+        {"MELI", "Ange", 2, 0, "Biyem-Assi", 21},
+        {"ZO'O", "Levi", 3, 1, "Essos", 25},
+        {"ABANDA", "Walter", 4, 1, "Mokolo", 22},
+        {"M", "Krys", 5, 2, "Ngousso", 20}
+    };
+
+    Candidat candidats[] = {
+        {"ZOLA", "Emile", 1, "Parti du Soleil", {12, 4, 1975}},
+        {"CONNOR", "Sarah", 2, "Parti du Lion", {5, 9, 1980}},
+        {"SAINT", "Jonas", 3, "Parti du Progrès", {22, 11, 1978}}
+    };
+
+    int tailleE = sizeof(electeurs) / sizeof(electeurs[0]);
+    int tailleC = sizeof(candidats) / sizeof(candidats[0]);
+
+    // ✅ Test du décompte des votes
+    ResultatVote resultat = compteVote(electeurs, tailleE, candidats, tailleC);
+
+    printf("=== Résultats du vote ===\n");
+    for (int i = 0; i < resultat.i; i++) {
+        printf("%s %s (%s) : %d votes (%f %%) \n",
+               resultat.candidat[i].prenom,
+               resultat.candidat[i].nom,
+               resultat.candidat[i].parti,
+               resultat.nbVote[i],
+               resultat.pourcent[i]);
+    }
+
+    // ✅ Test de la fonction aVote()
+    printf("\nTest de aVote() :\n");
+    printf("Dane -> %d\n", aVote(electeurs, tailleE,"POKEM", "Dane"));    //Ca retourne ici 1 si l'electeur  a voté, 0 s'il n'a pas voté et -1 si ca ne trouve pas le nom entré
+    printf("Meli -> %d\n", aVote(electeurs, tailleE, "MELI", "Ange"));
+    printf("Xavier -> %d\n", aVote(electeurs, tailleE, "TT", "Xavier"));
+
+    return 0;
+} */
